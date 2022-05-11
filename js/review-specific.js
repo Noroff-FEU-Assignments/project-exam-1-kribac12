e273f24a631b5fea58f1e5346b8c9d91f206891a
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-05-11 13:51:37 +0200

    API function on index.html and blogs.html

---
+++ js/review-specific.js
@@ -0,0 +1,41 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+console.log(id);
+
+const baseUrl = "http://fabulousfictio.wpengine.com/wp-json/wp/v2/review?acf_format=standard";
+
+console.log(baseUrl);
+
+const url = baseUrl + id;
+
+console.log(url);
+
+const reviewSpecificContainer = document.querySelector(".review-specific-container");
+
+async function showReview() {
+  try {
+    const response = await fetch(url);
+    const review = await response.json();
+
+    console.log(review);
+
+    document.title = "";
+    document.title = `${review.acf.heading}`;
+
+    reviewSpecificContainer.classList.remove("loader");
+
+    reviewSpecificContainer.innerHTML += `
+    <h1>${review.acf.heading}<h1>
+        <img src=${review.acf.image} alt ="${review.acf.heading}" class="review-img"/>
+        <h2 class="post-subheading">${review.acf.subheading}</h2>
+        <div class="synopsis"><h3>Synopsis:</h3> <p>${review.acf.synopsis1}</p><p>${review.acf.synopsis2}</p></div>
+       <div class="review-paragraph"><h3>Synopsis:</h3> <p>${review.acf.paragraph1}</p><p>${review.acf.paragraph2}</p><p>${review.acf.paragraph3}</p><p>${review.acf.paragraph4}</p></div>`;
+  } catch (error) {
+    console.log(error);
+    reviewSpecificContainer.innerHTML = "error";
+  }
+}
+
+showReview();
--- js/reviews.js
+++ js/reviews.js
@@ -1,14 +1,27 @@
 const reviewContainer = document.querySelector(".review-container");
 
-const reviewUrl = "https://fabulousfictio.wpengine.com/wp-json/wp/v2/review?acf_format=standard";
+const reviewUrl = "http://fabulousfictio.wpengine.com/wp-json/wp/v2/review?acf_format=standard";
 
 async function getReviews() {
   try {
-    const response = await fetch(reviewURL);
+    const response = await fetch(reviewUrl);
     const reviews = await response.json();
 
     reviewContainer.classList.remove("loader");
 
-    for (let i = 0; i < reviews.length; i++) {}
-  } catch {}
+    for (let i = 0; i < reviews.length; i++) {
+      reviewContainer.innerHTML += `<a href="blog-specific.html?id=${reviews[i].id}">
+      <img src=${reviews[i].acf.image} alt ="${reviews[i].acf.heading}" class="review-img"/>
+        <h3 class="heading-posts">${reviews[i].acf.book_title}</h3>
+        <p>Author: ${reviews[i].acf.author}</p>
+        <p>Genre: ${reviews[i].acf.genre}</p>
+        <p>Quote: "${reviews[i].acf.quote}"</p>
+        <div class=cta>Read review</div>
+        </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+getReviews();
