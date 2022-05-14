acbff054b26e041b13877f1bcb16891c02fbf7cb
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-05-14 14:06:22 +0200

    API and images

--- js/reviews.js
+++ js/reviews.js
@@ -10,13 +10,13 @@ async function getReviews() {
     reviewContainer.classList.remove("loader");
 
     for (let i = 0; i < reviews.length; i++) {
-      reviewContainer.innerHTML += `<a href="blog-specific.html?id=${reviews[i].id}">
+      reviewContainer.innerHTML += `<a href="blog-specific.html?id=${reviews[i].id}"><div class="review-card">
       <img src=${reviews[i].acf.image} alt ="${reviews[i].acf.heading}" class="review-img"/>
         <h3 class="heading-posts">${reviews[i].acf.book_title}</h3>
         <p>Author: ${reviews[i].acf.author}</p>
         <p>Genre: ${reviews[i].acf.genre}</p>
         <p>Quote: "${reviews[i].acf.quote}"</p>
-        <div class=cta>Read review</div>
+        <div class=cta>Read review</div> </div>
         </a>`;
     }
   } catch (error) {
