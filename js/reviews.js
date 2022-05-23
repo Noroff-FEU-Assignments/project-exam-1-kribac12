6f7004b9dacc5b67c7312bd0665d522ce356f904
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-05-23 14:37:49 +0200

    some font changes

--- js/reviews.js
+++ js/reviews.js
@@ -24,9 +24,9 @@ async function displayReviews() {
       reviewContainer.innerHTML += `<a href="blog-specific.html?id=${reviews[i].id}"><div class="review-card">
               <img src=${reviews[i].acf.image} alt ="${reviews[i].acf.heading}" class="review-img"/>
                 <h3 class="heading-posts">${reviews[i].acf.book_title}</h3>
-                <p>Author: ${reviews[i].acf.author}</p>
-                <p>Genre: ${reviews[i].acf.genre}</p>
-                <p>Quote: "${reviews[i].acf.quote}"</p>
+                <p class="review-card-p">Author: ${reviews[i].acf.author}</p>
+                <p class="review-card-p">Genre: ${reviews[i].acf.genre}</p>
+                <p class="review-card-p">Quote: "${reviews[i].acf.quote}"</p>
                 <div class=cta>Read review</div> </div>
                 </a>`;
     }
