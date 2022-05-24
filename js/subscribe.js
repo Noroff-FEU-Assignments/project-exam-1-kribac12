8e55cf6ea11b5e919d57f4f63b52fa7bd25f426f
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-05-24 17:05:51 +0200

    subscribe function js

---
+++ js/subscribe.js
@@ -0,0 +1,25 @@
+const email = document.querySelector("#email");
+const subscribeButton = document.querySelector("button");
+const subscribeForm = document.querySelector(".subscribe-form");
+const input = document.querySelector("input");
+
+function validateSubscription(form) {
+  form.preventDefault();
+
+  if (validateEmail(email.value) === true) {
+    subscribeButton.innerHTML = "Thank you! Check your inbox :)";
+  } else {
+    subscribeButton.innerHTML = "Please use a valid e-mail.";
+  }
+
+  subscribeForm.reset();
+}
+
+subscribeForm.addEventListener("submit", validateSubscription);
+
+function validateEmail(email) {
+  const regEx =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
